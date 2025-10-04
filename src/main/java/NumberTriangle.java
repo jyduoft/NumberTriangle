aee49aed0b1cf8cdfa0ee48b3eb0ab2328b1bf70
jdingutoronto <joseph.ding@mail.utoronto.ca>
2025-10-04 02:18:16 -0400

    finished NumberTriangle.retrieve

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -42,7 +42,6 @@ public class NumberTriangle {
         this.left = left;
     }
 
-
     public void setRight(NumberTriangle right) {
         this.right = right;
     }
@@ -88,7 +87,16 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
+        // DONETODO implement this method
+        if (path == null) {
+            return this.root;
+        }
+        else if (path.charAt(0) == 'l') {
+            return this.left.retrieve(path.substring(1));
+        }
+        else if (path.charAt(0) == 'r') {
+            return this.right.retrieve(path.substring(1));
+        }
         return -1;
     }
 
